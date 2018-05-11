0f2314470962a6abfd2282b8df493c283860cde7
yehchitsai <yehchitsai@gmail.com>
2018-05-11 11:47:19 +0800

    refine the variable name

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-		int i,j=10,s;
-	for(i=0,s=0;i<j;i++)
-		s=s+i;
-	printf("s=%d\n",s);
+	int init,endpoint=10,summary;
+	for(init=0,summary=0;init<endpoint;init++)
+		summary=summary+init;
+	printf("summary=%d\n",summary);
 	return 0;
 }
